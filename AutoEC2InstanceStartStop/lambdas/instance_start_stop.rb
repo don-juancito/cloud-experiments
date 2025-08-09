17df6fc1de56794f3d3a3a4fff77b823d8965096
Juan Luis Orozco Villalobos <jl.orozco.villa@gmail.com>
2025-08-09 16:31:10 +0200

    Update the lambda for handling state transition for instances

--- AutoEC2InstanceStartStop/lambdas/instance_start_stop.rb
+++ AutoEC2InstanceStartStop/lambdas/instance_start_stop.rb
@@ -1,4 +1,6 @@
+require 'aws-sdk-ec2'
 
+EC2 = Aws::EC2::Resource.new
 
 def handler(event:, context:)
   action = event['action']
@@ -7,5 +9,15 @@ def handler(event:, context:)
     raise "Invalid action: #{action}"
   end
 
-  puts action
+  tag_filter = { name: 'tag:Environment', values: ["development", "staging"] }
+
+  case action
+  when 'start'
+    EC2.instances(filters: [tag_filter, { name: 'instance-state-name', values: ['stopped'] }])
+       .each { |instance| instance.start }
+  when 'stop'
+    EC2.instances(filters: [tag_filter, { name: 'instance-state-name', values: ['running'] }])
+       .each { |instance| instance.stop }
+  end
+
 end
