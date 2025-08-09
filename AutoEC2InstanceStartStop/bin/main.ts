c5cb09b6929257963d14c56e6c69da300834661e
Juan Luis Orozco Villalobos <jl.orozco.villa@gmail.com>
2025-08-09 15:23:00 +0200

    Add a stack for creating test instances

--- AutoEC2InstanceStartStop/bin/main.ts
+++ AutoEC2InstanceStartStop/bin/main.ts
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AutoEc2InstanceStartStopStack } from '../lib/auto_ec2_instance_start_stop-stack';
+import {AutoEc2InstanceStartStopStack} from '../lib/auto_ec2_instance_start_stop-stack';
+import {Ec2TestInstancesStack} from "../lib/ec2_test_instances-stack";
 
 const app = new cdk.App();
-new AutoEc2InstanceStartStopStack(app, 'AutoEc2InstanceStartStopStack', {
-  
+new AutoEc2InstanceStartStopStack(app, 'AutoEc2InstanceStartStopStack', {});
+
+new Ec2TestInstancesStack(app, 'testInstances', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
+    devInstanceNumber: 4,
+    stagingInstanceNumber: 3,
+    prodInstanceNumber: 2,
 });
---
+++ AutoEC2InstanceStartStop/lib/ec2_test_instances-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {aws_ec2 as ec2} from "aws-cdk-lib";
+
+interface Ec2TestInstancesStackProps extends cdk.StackProps {
+    devInstanceNumber: number;
+    stagingInstanceNumber: number;
+    prodInstanceNumber: number;
+}
+
+export class Ec2TestInstancesStack extends cdk.Stack {
+    private readonly vpc: ec2.IVpc;
+
+    constructor(scope: Construct, id: string, props: Ec2TestInstancesStackProps) {
+        super(scope, id, props);
+
+        // Use the default VPC to avoid creating new networking resources.
+        this.vpc = ec2.Vpc.fromLookup(this, 'DefaultVpc', {isDefault: true});
+
+        this.createTestInstances(props.devInstanceNumber, "development");
+        this.createTestInstances(props.stagingInstanceNumber, "staging");
+        this.createTestInstances(props.prodInstanceNumber, "production");
+    }
+
+    createTestInstances(numberOfInstances: number, environment: string) {
+        for (let index = 0; index < numberOfInstances; index++) {
+            const instance = new ec2.Instance(this, `Instance-${environment}-${index + 1}`, {
+                instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+                machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+                vpc: this.vpc,
+            });
+
+            cdk.Tags.of(instance).add('Environment', environment);
+        }
+    }
+}
