4bbf86a5c06377d06857e4175305a431f7e55107
Juan Luis Orozco Villalobos <jl.orozco.villa@gmail.com>
2025-08-09 16:32:09 +0200

    Use single quotes consistently on both stacks, update the permissions for the state transition lambda

--- AutoEC2InstanceStartStop/lib/auto_ec2_instance_start_stop-stack.ts
+++ AutoEC2InstanceStartStop/lib/auto_ec2_instance_start_stop-stack.ts
@@ -1,42 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {aws_events as events} from "aws-cdk-lib";
-import {aws_events_targets as event_targets} from "aws-cdk-lib";
-import {aws_lambda as lambda} from "aws-cdk-lib";
-import {aws_sqs as sqs} from "aws-cdk-lib";
+import {aws_events as events} from 'aws-cdk-lib';
+import {aws_events_targets as event_targets} from 'aws-cdk-lib';
+import {aws_lambda as lambda} from 'aws-cdk-lib';
+import {aws_sqs as sqs} from 'aws-cdk-lib';
+import {aws_iam as iam} from 'aws-cdk-lib';
 
 export class AutoEc2InstanceStartStopStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const dlq = new sqs.Queue(this, "deadLetterQueue");
+        const dlq = new sqs.Queue(this, 'deadLetterQueue');
 
-        const startStopFunction = new lambda.Function(this, "startStopFunction", {
+        const startStopFunction = new lambda.Function(this, 'startStopFunction', {
             runtime: lambda.Runtime.RUBY_3_4,
-            code: lambda.Code.fromAsset("lambdas"),
-            handler: "instance_start_stop.handler",
-            description: "Starts and stops an EC2 instance",
+            code: lambda.Code.fromAsset('lambdas'),
+            handler: 'instance_start_stop.handler',
+            description: 'Starts and stops an EC2 instance',
         });
 
-        const startSchedule = new events.Rule(this, "startSchedule", {
-            schedule: events.Schedule.cron({weekDay: "MON-FRI", hour: "7", minute: "0"}),
+        const startSchedule = new events.Rule(this, 'startSchedule', {
+            schedule: events.Schedule.cron({weekDay: 'MON-FRI', hour: '7', minute: '0'}),
             targets: [new event_targets.LambdaFunction(startStopFunction, {
                 deadLetterQueue: dlq,
                 maxEventAge: cdk.Duration.minutes(5),
                 retryAttempts: 2,
-                event: events.RuleTargetInput.fromObject({action: "start"}),
+                event: events.RuleTargetInput.fromObject({action: 'start'}),
             })]
         });
 
-        const stopSchedule = new events.Rule(this, "stopSchedule", {
-            schedule: events.Schedule.cron({weekDay: "MON-FRI", hour: "15", minute: "0"}),
+        const stopSchedule = new events.Rule(this, 'stopSchedule', {
+            schedule: events.Schedule.cron({weekDay: 'MON-FRI', hour: '15', minute: '0'}),
             targets: [new event_targets.LambdaFunction(startStopFunction, {
                 deadLetterQueue: dlq,
                 maxEventAge: cdk.Duration.minutes(5),
                 retryAttempts: 2,
-                event: events.RuleTargetInput.fromObject({action: "stop"}),
+                event: events.RuleTargetInput.fromObject({action: 'stop'}),
             })]
         });
 
+        const ec2ManagementPolicy = new iam.PolicyStatement({
+            actions: ['ec2:DescribeInstances', 'ec2:StartInstances', 'ec2:StopInstances'],
+            resources: ['*'],
+        });
+        startStopFunction.addToRolePolicy(ec2ManagementPolicy);
     }
 }
--- AutoEC2InstanceStartStop/lib/ec2_test_instances-stack.ts
+++ AutoEC2InstanceStartStop/lib/ec2_test_instances-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {aws_ec2 as ec2} from "aws-cdk-lib";
+import {aws_ec2 as ec2} from 'aws-cdk-lib';
 
 interface Ec2TestInstancesStackProps extends cdk.StackProps {
     devInstanceNumber: number;
@@ -9,25 +9,24 @@ interface Ec2TestInstancesStackProps extends cdk.StackProps {
 }
 
 export class Ec2TestInstancesStack extends cdk.Stack {
-    private readonly vpc: ec2.IVpc;
 
     constructor(scope: Construct, id: string, props: Ec2TestInstancesStackProps) {
         super(scope, id, props);
 
         // Use the default VPC to avoid creating new networking resources.
-        this.vpc = ec2.Vpc.fromLookup(this, 'DefaultVpc', {isDefault: true});
+        const defaultVpc = ec2.Vpc.fromLookup(this, 'DefaultVpc', {isDefault: true});
 
-        this.createTestInstances(props.devInstanceNumber, "development");
-        this.createTestInstances(props.stagingInstanceNumber, "staging");
-        this.createTestInstances(props.prodInstanceNumber, "production");
+        this.createTestInstances(props.devInstanceNumber, 'development', defaultVpc);
+        this.createTestInstances(props.stagingInstanceNumber, 'staging', defaultVpc);
+        this.createTestInstances(props.prodInstanceNumber, 'production', defaultVpc);
     }
 
-    createTestInstances(numberOfInstances: number, environment: string) {
+    createTestInstances(numberOfInstances: number, environment: string, vpc: ec2.IVpc) {
         for (let index = 0; index < numberOfInstances; index++) {
             const instance = new ec2.Instance(this, `${environment}-${index + 1}`, {
                 instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
                 machineImage: ec2.MachineImage.latestAmazonLinux2023(),
-                vpc: this.vpc,
+                vpc: vpc,
             });
 
             cdk.Tags.of(instance).add('Environment', environment);
