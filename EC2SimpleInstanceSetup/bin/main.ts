6c14bf1b1ee9b6c158e55d2d14b5e1602fda0a29
Juan Luis Orozco Villalobos <jl.orozco.villa@gmail.com>
2025-09-09 18:57:40 +0200

    Remove simple instance EC2 lab skeleton

--- EC2SimpleInstanceSetup/bin/main.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { Ec2SimpleInstanceSetupStack } from '../lib/ec2_simple_instance_setup-stack';
-
-const app = new cdk.App();
-new Ec2SimpleInstanceSetupStack(app, 'Ec2SimpleInstanceSetupStack', {
-});
